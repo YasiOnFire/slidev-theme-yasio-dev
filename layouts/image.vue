5027a9e5750e38bdfed129a9be1f948dd6610e5a
Yasio <baszczok@gmail.com>
2023-06-27 19:03:25 +0200

    add image template

---
+++ layouts/image.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { computed } from 'vue';
+import { handleBackground } from './helpers';
+
+const props = defineProps({
+  image: {
+    type: String,
+  },
+  contain: {
+    type: Boolean,
+    default: false,
+  }
+})
+
+const style = computed(() => handleBackground(props.image))
+</script>
+
+<template>
+  <div class="slidev-layout w-full h-full" :style="{...style, backgroundSize: contain ? 'contain' : 'cover'}">
+    <slot />
+  </div>
+</template>
